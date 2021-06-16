5b4ff6a16d3db07b77976823484b2ca280063ed2
macascarp <maca.scarp@gmail.com>
2021-06-16 11:49:24 -0500

    cards on

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BodyComponent } from './body/body.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const app_routes: Routes = [
+  {path: '', component: BodyComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: '**', pathMatch: 'full', redirectTo: ''},
+]
+
+
+
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot( app_routes )
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+/* RUTAS */
 import { AppRoutingModule } from './app-routing.module';
+
+/* COMPONENTES */
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BodyComponent,
-    FooterComponent
+    FooterComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
