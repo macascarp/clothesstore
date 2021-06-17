b46e55386757ca2f169901e2fb2b5cb90110cec8
macascarp <maca.scarp@gmail.com>
2021-06-17 16:32:09 -0500

    desplegado en Docker Hub

--- src/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-var botonElement = document.getElementsByClassName(".suma-cart");
-            var pElement = document.getElementsByClassName(".cart-counter");
-            var contador = 0;
-            botonElement.onclick = function () {
-                contador++;
-                pElement.textContent = contador;
-            }
